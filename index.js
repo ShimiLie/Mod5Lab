8a15703111cb583a12da893ff746288bf3c6fa2e
alie <anthonylie94@gmail.com>
2023-01-12 18:19:28 +0800

    installed nodemon

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const express =require("express")
+const app = express();
+const port = 3000;
+const port2 = 8000;
+
+
+app.get("/", (req, res) => { 
+    res.send("Welcoome!")
+});
+
+app.get("/wishlist", (req, res) => { 
+    res.send("My wishlist!")
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Local host ${port}`)
+});
+
+app.listen(port2, () => {
+    console.log(`Local host ${port2}`)
+});
+
